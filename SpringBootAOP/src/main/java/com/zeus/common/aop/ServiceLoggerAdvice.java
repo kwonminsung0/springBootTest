88603258b14a6fc0c7eb985669e410f509c70941
minsung <minsung21c@gmail.com>
2025-01-16 17:31:10 +0900

    스프링부트 AOP 테스팅

---
+++ SpringBootAOP/src/main/java/com/zeus/common/aop/ServiceLoggerAdvice.java
@@ -0,0 +1,72 @@
+package com.zeus.common.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Aspect
+public class ServiceLoggerAdvice {
+
+	// JoinPoint(핵심코드) 실행전에 작동
+	/*
+	 * @Before("execution(* com.zeus.service.BoardService*.*(..))") public void
+	 * startLog(JoinPoint jp) { log.info("ServiceLoggerAdvice.startLog");
+	 * log.info("ServiceLoggerAdvice.startLog jp.signature" + jp.getSignature());
+	 * log.info("ServiceLoggerAdvice.startLog jp." + Arrays.toString(jp.getArgs()));
+	 * log.info("================================================================");
+	 * }
+	 */
+
+	// JoinPoint(핵심코드) 실행후에 작동
+	/*
+	 * @AfterReturning(pointcut =
+	 * "execution(* com.zeus.service.BoardService*.*(..))", returning = "result")
+	 * public void stopLog(JoinPoint jp, Object result) {
+	 * log.info("ServiceLoggerAdvice.stopLog"); if (result != null) {
+	 * log.info("ServiceLoggerAdvice.stopLog result" + result.toString()); }
+	 * log.info("****************************************************************");
+	 * }
+	 */
+	// JoinPoint(핵심코드) 예외가 발생했을때 작동
+	@AfterThrowing(pointcut = "execution(* com.zeus.service.BoardService*.*(..))", throwing = "e")
+	public void exceptionLog(JoinPoint jp, Exception e) {
+		log.info("ServiceLoggerAdvice.exceptionLog");
+		if (e != null) {
+			log.info("ServiceLoggerAdvice.exceptionLog exception" + e.toString());
+		}
+		log.info("****************************************************************");
+	}
+
+	// JoinPoint(핵심코드) 전, 후 작동
+	@Around("execution(* com.zeus.service.BoardService*.*(..))")
+	public Object timeLog(ProceedingJoinPoint pjp) throws Throwable {
+		// 앞에서 advice 실행한다.
+		long startTime = System.currentTimeMillis();
+		// JoinPoint(핵심코드) 실행한다.
+		Object obj = null;
+		try {
+			obj = pjp.proceed();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// 뒤에서 advice 실행한다.
+		long stopTime = System.currentTimeMillis();
+		// 부산물 정보 획득
+		log.info(pjp.getSignature().getName() + ":" + (stopTime - startTime));
+		log.info("---------------------------------------------------------");
+		return obj;
+	}
+
+}
