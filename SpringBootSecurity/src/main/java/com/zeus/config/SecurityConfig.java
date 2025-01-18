d76129009a8860c7a92d7a5f2f5ed0897c280e10
minsung <minsung21c@gmail.com>
2025-01-18 15:37:33 +0900

    스프링부트 스프링시큐리티 기능 테스팅

--- SpringBootSecurity/src/main/java/com/zeus/config/SecurityConfig.java
+++ SpringBootSecurity/src/main/java/com/zeus/config/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.zeus.common.security.CustomAccessDeniedHandler;
+import com.zeus.common.security.CustomLoginSuccessHandler;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,24 +23,28 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		log.info("SecurityConfig");
-		// 1. csrf 토큰 비활성화
+		// csrf 토큰 비활성화
 		http.csrf().disable();
 
-		// 2. /board/list 인증 ok, /board/register 인증, 인가(MEMBER)
+		// URI 패턴으로 모든 접근 제한을 설정한다.
 		http.authorizeRequests().requestMatchers("/board/list").permitAll();
+		http.authorizeRequests().requestMatchers("/notice/list").permitAll();
+
 		// 로그인하고 멤버 인가받은사람만 가능
 		http.authorizeRequests().requestMatchers("/board/register").hasRole("MEMBER");
-		http.authorizeRequests().requestMatchers("/notice/list").permitAll();
 		// 로그인하고 관리자로 인가받은사람만 가능
 		http.authorizeRequests().requestMatchers("/notice/register").hasRole("ADMIN");
-		
-		// 3. id, password 기존것을 사용하지 않고 우리가 설계한 아이디와 패스워드, 인가정책 세워서 제시하겠다.
-		// 4. 아이디나 패스워드 잘못되었을때 화면에 인증이 안됩니다. 다시 입력하세요.(화면)
-		http.exceptionHandling().accessDeniedPage("/accessError");
-		// 5. 로그인 기본폼을 사용
-		http.formLogin();
 
-		
+		// http.exceptionHandling().accessDeniedPage("/accessError");
+		// 등록한 CustomAccessDeniedHandler.java를 접근 거부 처리자로 지정한다.
+		http.exceptionHandling().accessDeniedHandler(createAccessDeniedHandler());
+
+		// 개발자가 정의한 로그인 페이지의 URI를 지정한다.
+		// 로그인 성공 후 처리를 담당하는 처리자로 지정한다.
+		http.formLogin().loginPage("/login").successHandler(createAuthenticationSuccessHandler());
+
+		// 로그아웃 처리를 위한 URI를 지정하고, 로그아웃한 후에 세션을 무효화 한다.
+		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
 
 		return http.build();
 	}
@@ -45,5 +54,17 @@ public class SecurityConfig {
 		// 지정된 아이디와 패스워드로 로그인이 가능하도록 설정한다.
 		auth.inMemoryAuthentication().withUser("member").password("{noop}1234").roles("MEMBER");
 		auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("ADMIN");
+	}
+
+	// CustomAccessDeniedHandler를 빈으로 등록한다.
+	@Bean
+	public AccessDeniedHandler createAccessDeniedHandler() {
+		return new CustomAccessDeniedHandler();
+	}
+
+	// CustomLoginSuccessHandler를 빈으로 등록한다.
+	@Bean
+	public AuthenticationSuccessHandler createAuthenticationSuccessHandler() {
+		return new CustomLoginSuccessHandler();
 	}
 }
